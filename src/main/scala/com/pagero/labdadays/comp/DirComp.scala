3130fda6c0fe86a1d8ce31bc540048a582c0b738
eranga herath <eranga.herath@pagero.com>
2016-09-02 11:38:55 +0530

    added dir trait

--- src/main/scala/com/pagero/labdadays/comp/DirComp.scala
+++ src/main/scala/com/pagero/labdadays/comp/DirComp.scala
@@ -4,6 +4,8 @@ import java.io.{File, FileFilter}
 
 import com.pagero.labdadays.config.Configuration
 
+case class Dir[A](path: String, content: A)
+
 object DirComp extends Configuration {
   def rootIter(dirs: List[File], dirIter: File => Unit): Unit = {
     dirs match {
